690a41e0defaaecac30f8c4f3871064a0ce3c134
unknown <gaviriayeferson2@gmail.com>
2025-03-27 09:37:47 +0100

    Add initial  test for Account constructor

---
+++ src/test/java/dev/yeferson/AccountTest.java
@@ -0,0 +1,20 @@
+package dev.yeferson;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AccountTest {
+
+    @Test
+
+    public void testAccountConstructor() {
+        Account account = new Account(10000.0, 5.0);
+        assertThat(account.getBalance(), equalTo(10000.0));
+        assertThat(account.getAnnualInterestRate(), equalTo(5.0));
+        assertThat(account.getNumberOfDeposits(), equalTo(0));
+        assertThat(account.getNumberOfWithdrawals(), equalTo(0));
+        assertThat(account.getMonthlyCommission(), equalTo(0.0));
+    }
+
+}
