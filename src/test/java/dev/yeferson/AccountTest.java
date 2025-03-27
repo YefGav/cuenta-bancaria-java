b334a1dbb2a016e25c0afd678d99403fdda3cc97
unknown <gaviriayeferson2@gmail.com>
2025-03-27 09:57:16 +0100

    Refactor Account tests to use @BeforeEach for shared account instance

--- src/test/java/dev/yeferson/AccountTest.java
+++ src/test/java/dev/yeferson/AccountTest.java
@@ -1,21 +1,33 @@
 package dev.yeferson;
 
 import dev.yeferson.model.Account;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AccountTest {
 
+    private Account account;
+
+    @BeforeEach
+    public void setUp() {
+        account = new Account(5000.0, 3.0);
+    }
     @Test
 
     public void testAccountConstructor() {
-        Account account = new Account(10000.0, 5.0);
-        assertThat(account.getBalance(), equalTo(10000.0));
-        assertThat(account.getAnnualInterestRate(), equalTo(5.0));
+        Account account = new Account(5000.0, 3.0);
+        assertThat(account.getBalance(), equalTo(5000.0));
+        assertThat(account.getAnnualInterestRate(), equalTo(3.0));
         assertThat(account.getNumberOfDeposits(), equalTo(0));
         assertThat(account.getNumberOfWithdrawals(), equalTo(0));
         assertThat(account.getMonthlyCommission(), equalTo(0.0));
     }
+
+    
+
+  
 
 }
