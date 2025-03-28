31215e28c5940bd6bfbfbd662920965e7a494290
unknown <gaviriayeferson2@gmail.com>
2025-03-28 09:44:52 +0100

    Add  test for withdraw in active SavingsAccount that deactivates account

--- src/test/java/dev/yeferson/model/SavingAccountTest.java
+++ src/test/java/dev/yeferson/model/SavingAccountTest.java
@@ -6,46 +6,55 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class SavingAccountTest {
-    
-        private SavingAccount account;
-    
-        @BeforeEach
-        public void setUp() {
-            account = new SavingAccount(5000.0, 3.0);
-        }
-    
-        @Test
-        public void testAccountIsInactive() {
-            assertThat(account.isActive(), equalTo(false));
-        }
 
-        
+    private SavingAccount account;
+
+    @BeforeEach
+    public void setUp() {
+        account = new SavingAccount(5000.0, 3.0);
+    }
+
+    @Test
+    public void testAccountIsInactive() {
+        assertThat(account.isActive(), equalTo(false));
+    }
+
     @Test
     public void testDepositActivateAccount() {
-      
-        account.deposit(6000.0); 
+
+        account.deposit(6000.0);
         assertThat(account.getBalance(), equalTo(11000.0));
         assertThat(account.isActive(), equalTo(true));
         assertThat(account.getNumberOfDeposits(), equalTo(1));
     }
 
     public class SavingsAccountWithdrawTest {
-        
+
         private SavingAccount account;
-        
+
         @BeforeEach
         public void setUp() {
 
             account = new SavingAccount(15000.0, 3.0);
         }
-        
+
         @Test
         public void testWithdrawRemainsActive() {
-           
+
             account.withdraw(5000.0);
             assertThat(account.getBalance(), equalTo(10000.0));
             assertThat(account.isActive(), equalTo(true));
             assertThat(account.getNumberOfWithdrawals(), equalTo(1));
         }
+
+        @Test
+        public void testWithdrawDeactivatesAccount() {
+
+            account.withdraw(6000.0);
+            assertThat(account.getBalance(), equalTo(9000.0));
+            assertThat(account.isActive(), equalTo(false));
+            assertThat(account.getNumberOfWithdrawals(), equalTo(1));
+        }
     }
+
 }
