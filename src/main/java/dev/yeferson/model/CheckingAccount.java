0cd825f7c07c04d8130829221d9168c571d9fde6
unknown <gaviriayeferson2@gmail.com>
2025-03-28 10:30:21 +0100

    Implement withdraw method in CheckingAccount to support overdraft

--- src/main/java/dev/yeferson/model/CheckingAccount.java
+++ src/main/java/dev/yeferson/model/CheckingAccount.java
@@ -2,8 +2,37 @@ package dev.yeferson.model;
 
 public class CheckingAccount extends Account {
 
+    private double overdraft;
+
     public CheckingAccount(double balance, double annualInterestRate) {
+
         super(balance, annualInterestRate);
+        
     }
 
+    public CheckingAccount(double balance, double annualInterestRate, double overdraft) {
+        super(balance, annualInterestRate);
+        this.overdraft = 0.0;
+    }
+
+    public double getOverdraft() {
+        return overdraft;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        
+        if (amount > this.balance) {
+            double overdraftNeeded = amount - this.balance;
+            this.overdraft += overdraftNeeded;
+            
+            this.balance -= amount;
+        } else {
+          
+            this.balance -= amount;
+        }
+        this.numberOfWithdrawals++;
+    }
+
+    
 }
